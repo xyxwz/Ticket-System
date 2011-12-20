d92668005fb923e47e94c4951fdf4f86dbcbeeb1
vanetix <matt.mcfarland@txstate.edu>
2011-12-20 16:14:29 -0600

    added mustache template namespace, renders

---
+++ client/js/templates.js
@@ -0,0 +1,49 @@
+/* Namespace of all used templates, each takes
+ * in a object and returns a rendered template.
+ * ex: renders.NewTicket(this.toJSON()); */
+
+var renders = renders || {};
+
+(function() {
+
+  renders.NewTicket = function(ticket) {
+
+    var template = '';
+
+    return Mustache.to_html(template, ticket);
+
+  };
+
+  renders.DisplayTicket = ''function(ticket) {
+
+    var template = '';
+
+    return Mustache.to_html(template, ticket);
+
+  };
+
+  renders.ListTicket = function(ticket) {
+    
+    var template = '';
+    
+    return Mustache.to_html(template, ticket);
+
+  };                   
+
+  renders.newComment = function(comment) {
+
+    var template = '';
+
+    return Mustache.to_html(template, comment);
+
+  };                   
+
+  renders.DisplayComment = function(comment) {
+    
+    var template = '';
+
+    return Mustache.to_html(template, comment);
+
+  };
+
+}).call(this);
