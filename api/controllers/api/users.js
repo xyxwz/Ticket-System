a2bae0ae6427104f602aa77f7ba131033ab7ec40
Cody Stoltman <particlebanana@gmail.com>
2011-12-19 19:51:36 -0600

    move user logic to models

--- api/controllers/api/users.js
+++ api/controllers/api/users.js
@@ -6,15 +6,12 @@ module.exports = function(app) {
    
   /* User Index
   *  GET /users.json
+  *
   *  returns a list of all the users in the database */
   app.get('/users.json', function(req, res) {
     User.getAll(function(err, users){
-      if(err) {
-        res.json({error: 'Error getting users'}, 400);
-      }
-      else {
-        res.json(users);
-      }
+      if(err) return res.json({error: 'Error getting users'}, 400);
+      res.json(users);
     });
   });
   
@@ -30,100 +27,71 @@ module.exports = function(app) {
   *  adds a user to the database */
   app.post('/users.json', function(req, res) {
     var data = req.body;
-    var user = new User({
-      email: data.email,
-      name: data.name,
-      department: data.department
-    });
-    user.save(function(err, model) {
-      if (err || !model) {
-        res.json({error: 'Missing required attributes'}, 400);
-      } 
-      else {
-        res.json(model.toClient());
-      }
+    User.create(data, function(err, model) {
+      if(err) return res.json({error: 'Missing required attributes'}, 400);
+      res.json(model);
     });
   });
   
   
   /* User Info
-  *  GET /users/:id.json
+  *  GET /users/:userID.json
   *
-  *  id - The MongoDb BSON id converted to a string
+  *  userID - The MongoDb BSON id converted to a string
   *
   *  returns a single user */
-  app.get('/users/:id.json', function(req, res) {
-    User
-    .findOne({'_id': req.params.id})
-    .run(function(err, user) {
-      if (err || !user) {
-        res.json({error: 'User not found'}, 404);
-      } 
-      else {
-        res.json(user.toClient());
-      }
-    });
+  app.get('/users/:userID.json', function(req, res) {
+    res.json(req.user.toClient());
   });
   
   
   /* Update a user
-  *  PUT /users/:id.json
+  *  PUT /users/:userID.json
   *
-  *  id   - The MongoDb BSON id converted to a string
+  *  userID   - The MongoDb BSON id converted to a string
   *  body - The attributes to update in the model
   *        :email      - string
   *        :name       - string in format "first last"
   *        :department - string, used to determine permissions
   *
   *  updates the user instance with the passed in attributes */
-  app.put('/users/:id.json', function(req, res) {
+  app.put('/users/:userID.json', function(req, res) {
     var data = req.body;
-    User
-    .findOne({'_id': req.params.id})
-    .run(function(err, user) {
-      if (err || !user) {
-        res.json({error: 'User not found'}, 404);
-      } 
-      else {
-        if (data.email) user.email = data.email;
-        if (data.name) user.name = data.name;
-        if (data.department) user.department = data.department;
-        user.save(function(err, model) {
-          if (err || !model) {
-            return res.json({error: 'Error updating model. Check required attributes.'}, 400);
-          } 
-          else {
-            res.json(model.toClient());
-          }
-        });
-      }
+    var user = req.user;
+    user.update(data, function(err, model) {
+      if(err) return res.json({error: 'Error updating user'});
+      res.json(model);
     });
   });
   
   
   /* Delete a user
-  *  DELETE /users/:id.json
+  *  DELETE /users/:userID.json
   *
-  *  id - The MongoDb BSON id converted to a string
+  *  userID - The MongoDb BSON id converted to a string
   *
   *  removes a user from the database */
-  app.del('/users/:id.json', function(req, res) {
+  app.del('/users/:userID.json', function(req, res) {
+    var user = req.user;
+    user.removeUser(function(err, status) {
+      if(err) return res.json({error: 'Error removing user'});
+      res.json({success: "ok"});
+    });
+  });
+
+
+  /* ---------------------------------------------- *
+   * Pre-conditions
+   * ---------------------------------------------- */
+
+  /* Find A User */
+  app.param('userID', function(req, res, next, id){
     User
-    .findOne({'_id': req.params.id})
+    .findOne({'_id':id})
     .run(function(err, user) {
-      if (err || !user) {
-        res.json({error: 'User not found'}, 404);
-      } 
-      else {
-        user.remove(function(err) {
-          if (err) {
-            res.json({error: 'Error removing user'}, 400);
-          } 
-          else {
-            res.json({success: 'ok'});
-          }
-        });
-      }
+      if(err || !user) return res.json({error: 'User not found'}, 404);
+      req.user = user;
+      next();
     });
   });
   
--- api/models/user.js
+++ api/models/user.js
@@ -10,6 +10,16 @@ var User = new mongoose.Schema({
   created       : {type : Date,   default : Date.now, required: true}
 });
 
+
+/* ---------------------------------------- *
+ * Instance Methods *
+ * ---------------------------------------- */
+
+
+/* To Client *
+*
+*  Returns an object for use with client-side js.
+*  Sets the mongo _id property name to id */
 User.methods.toClient = function(){
   var obj = this.toObject();
   obj.id = obj._id;
@@ -17,16 +27,111 @@ User.methods.toClient = function(){
   return obj;
 }
 
-User.statics.getAll = function(callback){
-  this.find().run(function(err, models){
-    if(err) return callback(err);
-    var array = [];
-    _.each(models, function(user) {
-      var obj = user.toClient();
-      array.push(obj);
-    });
-    return callback(null, array);
+
+/* Update User *
+*
+*  data - A json object representing user properties to update
+*     :name         - string, optional
+*     :email        - string, optional
+*     :department   - string, optional, available options ['IT', 'K12']
+*
+*  Updates a user and returns a user object
+*  ready to be sent to the client. */
+User.methods.update = function(data, callback) {
+  if (data.email) this.email = data.email;
+  if (data.name) this.name = data.name;
+  if (data.department) this.department = data.department;
+  this.save(function(err, model) {
+    if (err || !model) {
+      callback('Error updating model. Check required attributes.');
+    }
+    else {
+      return callback(null, model.toClient());
+    }
   });
 }
+
+
+/* Delete User *
+*
+*  Removes a user from the database.
+*  Returns an error or status "ok" to the callback */
+User.methods.removeUser = function(callback) {
+  this.remove(function(err) {
+    if (err) return callback('Error removing user');
+    return callback(null, "ok");
+  });
+}
+
+
+/* ---------------------------------------- *
+ * Static Methods *
+ * ---------------------------------------- */
+
+
+/* Return All Users
+*
+*
+*  Gets a list of all the users in the database.
+*
+* Returns an Array ready to be sent to the client. */
+User.statics.getAll = function(callback){
+  this.find().run(function(err, models){
+    if(err || !models) {
+      return callback("Error finding users");
+    }
+    else {
+      var array = [];
+      _.each(models, function(user) {
+        var obj = user.toClient();
+        array.push(obj);
+      });
+      return callback(null, array);
+    }
+  });
+}
+
+
+/* Return A Single User
+*
+*  :id - string, a user's BSON id
+*
+*  Return a single user object ready to be sent to the client */
+User.statics.getSingle = function(id, callback){
+  this
+  .findOne({'_id':id})
+  .run(function(err, model){
+    if(err || !model) {
+      return callback("User not found");
+    }
+    else {
+      return callback(null, model.toClient());
+    }
+  });
+}
+
+
+/* Create A User *
+*
+*  data - A json object representing user properties
+*     :name       - string
+*     :email      - string
+*     :department - string, options include ['IT', 'K12']
+*
+*  Creates a new user.
+*  Returns a user object ready to be sent to the client. */
+User.statics.create = function(data, callback) {
+  var self = this;
+  var user = new self({
+    email: data.email,
+    name: data.name,
+    department: data.department
+  });
+  user.save(function(err, model) {
+    if (err || !model) return callback("Error saving user");
+    return callback(null, model.toClient());
+  });
+}
+
 
 mongoose.model('User', User);
