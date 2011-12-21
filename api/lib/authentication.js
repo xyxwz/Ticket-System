0029ac511076fb5e4687cdd00f3a8a3e4e845f1c
Cody Stoltman <particlebanana@gmail.com>
2011-12-20 22:21:07 -0600

    whoops rename token header to "Auth-Token"

--- api/lib/authentication.js
+++ api/lib/authentication.js
@@ -10,8 +10,8 @@ var models = require('../models'),
  * Checks the value of the given X-CSRF-Token header
  * compared to the value in the database for a user */
 exports.Auth = function(req, res, next) {
-  if(typeof(req.header('X-CSRF-Token')) != 'undefined') {
-    var token = req.header('X-CSRF-Token');
+  if(typeof(req.header('Auth-Token')) != 'undefined') {
+    var token = req.header('Auth-Token');
     User
     .findOne({'access_token':token})
     .run(function(err, model){
