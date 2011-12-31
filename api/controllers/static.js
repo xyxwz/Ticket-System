c55eb98a28259df9283bc23812e5ee9a2388ddaf
Cody Stoltman <particlebanana@gmail.com>
2011-12-31 17:45:47 -0600

    bootstrap tickets collection when initialized from static json rendered server side
    	- This prevents a fetch on page load

--- api/controllers/static.js
+++ api/controllers/static.js
@@ -1,3 +1,6 @@
+var mongoose = require('mongoose'),
+    Ticket = mongoose.model('Ticket');
+
 module.exports = function(app) {
   
   /* Site Index
@@ -7,7 +10,15 @@ module.exports = function(app) {
    */
   app.get('/', function(req, res) {
     if(req.session.passport.user) {
-      res.render('index', {token: req.session.passport.user});
+      // Get open tickets to bootstrap into page.
+      // This prevents a fetch on page load.
+      Ticket.getAll('open', function(err, tickets){
+        if(err) return res.json({error: 'Error getting tickets'}, 400);
+        res.render('index', {
+          token: req.session.passport.user,
+          tickets: JSON.stringify(tickets),
+        });
+      });
     }
     else {
       res.redirect('/login'); 
