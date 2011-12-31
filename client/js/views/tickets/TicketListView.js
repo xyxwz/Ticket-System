17747247e09603f34336d28800a8cf090e4dffd0
Cody Stoltman <particlebanana@gmail.com>
2011-12-31 16:09:22 -0600

    add ticket index views and bind to collection reset

---
+++ client/js/views/tickets/TicketListView.js
@@ -0,0 +1,31 @@
+/* TicketListView
+ * Renders a collection of Tickets
+ */
+
+define(['jquery', 'underscore', 'backbone', 'views/tickets/TicketView'], 
+function($, _, Backbone, TicketView) {
+
+  var TicketListView = Backbone.View.extend({
+    el: $('#main'),
+
+    initialize: function() {
+      _.bindAll(this, "renderList");
+      this.collection.bind("reset", this.renderList);
+    },
+
+    renderList: function() {
+      var collection = this.collection,
+          self = this;
+
+      _.each(collection.models, function(ticket) {
+        var view = new TicketView({
+          model: ticket,
+        });
+        $(self.el).append(view.render().el);
+      });
+    },
+
+  });
+
+  return TicketListView;
+ });
