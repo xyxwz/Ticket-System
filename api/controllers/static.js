90323e871cfa67033a1f3a9cdf2bf861e98f3a80
Cody Stoltman <particlebanana@gmail.com>
2012-01-02 09:28:25 -0600

    Bootstrap closed tickets server-side along with open tickets
    	- TODO: paginate results to limit initial collection sizes

--- api/controllers/static.js
+++ api/controllers/static.js
@@ -10,17 +10,32 @@ module.exports = function(app) {
    */
   app.get('/', function(req, res) {
     if(req.session.passport.user) {
-      // Get open tickets to bootstrap into page.
-      // This prevents a fetch on page load.
-      Ticket.getAll('open', function(err, tickets){
+
+      /* If User session then user has been authenticated.
+       * Get tickets to bootstrap into page.
+       * This prevents a fetch on page load. */
+      Ticket.getAll('open', function(err, models) {
         if(err) return res.json({error: 'Error getting tickets'}, 400);
-        res.render('index', {
-          token: req.session.passport.user,
-          tickets: JSON.stringify(tickets),
+        var openTickets = models;
+
+        Ticket.getAll('closed', function(err, models) {
+          if(err) return res.json({error: 'Error getting tickets'}, 400);
+          var closedTickets = models;
+
+          // Render Index with bootstrapped tickets
+          res.render('index', {
+            token: req.session.passport.user,
+            openTickets: JSON.stringify(openTickets),
+            closedTickets: JSON.stringify(closedTickets),
+          });
+
         });
       });
     }
     else {
+      /* If no session the user is not authenticated
+       * redirect to login page to allow oAuth authentication.
+       * TODO: Add error message on unsuccessfull authentication */
       res.redirect('/login'); 
     }
   });
