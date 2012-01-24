c9b3e93d86ee54870e76fc91766d12b951d95058
Matt McFarland <matmcfarland@gmail.com>
2012-01-24 16:38:43 -0600

    build script now cleans up garbage

--- client/js/build.sh
+++ client/js/build.sh
@@ -6,4 +6,11 @@ if [[ -d 'release/' ]]; then
   rm -rf 'release/'
 fi
 
+
 node libs/r.js -o app.build.js
+
+
+if [[ $? == 0 ]]; then
+	mv 'release/libs/require/require-jquery.js' 'release/'
+	find release/* \( -iname "*" ! -iname "main.js" ! -iname "require-jquery.js" \) -delete
+fi
