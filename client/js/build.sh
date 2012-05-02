890e89f7daaa814ac2567e813f1adb8b20501068
Cody Stoltman <particlebanana@gmail.com>
2012-05-02 17:26:33 -0500

    update the timestamp for the appcache in the build tool

--- client/js/build.sh
+++ client/js/build.sh
@@ -9,6 +9,13 @@ fi
 
 node libs/r.js -o app.build.js
 
+# Update timestamp on manifest.appcache
+a="# "
+b=`date +%s`
+now=$a$b
+
+sed "2 s/# [0-9]*/$now/" ../manifest.appcache > tmp.appcache;mv tmp.appcache ../manifest.appcache
+
 
 if [[ $? == 0 ]]; then
 	mv 'release/libs/require/require-jquery.js' 'release/'
