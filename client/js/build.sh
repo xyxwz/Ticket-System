2b68c24a90216d199424c91d7fa094fc7bc2add7
Matt McFarland <matmcfarland@gmail.com>
2012-12-17 16:06:38 -0600

    clean up build script

--- client/js/build.sh
+++ client/js/build.sh
@@ -6,18 +6,18 @@ if [[ -d 'release/' ]]; then
   rm -rf 'release/'
 fi
 
-
-node libs/r.js -o app.build.js
-
-# Update timestamp on manifest.appcache
-a="# "
-b=`date +%s`
-now=$a$b
-
-sed "2 s/# [0-9]*/$now/" ../manifest.appcache > tmp.appcache;mv tmp.appcache ../manifest.appcache
+echo "Building source with r.js"
+node libs/r.js -o app.build.js &> /dev/null
 
 
 if [[ $? == 0 ]]; then
-	mv 'release/libs/require/require-jquery.js' 'release/'
-	find release/* \( -iname "*" ! -iname "main.js" ! -iname "require-jquery.js" \) -delete
+  # Update timestamp on manifest.appcache
+  now="# `date +%s`"
+  echo "Updating appcache with new timestamp -`echo $now | sed 's/#//'`"
+  sed -i "2 s/# [0-9]*/$now/" ../manifest.appcache
+
+  # Move require, and remove all unneeded files
+  echo "Removing unnecessary files from ./release"
+  mv 'release/libs/require/require-jquery.js' 'release/'
+  find release/* \( -iname "*" ! -iname "main.js" ! -iname "require-jquery.js" \) -delete
 fi
