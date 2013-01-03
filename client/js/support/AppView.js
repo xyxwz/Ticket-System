2b349a1465414938958af0682852eebe7631096c
Matt McFarland <matmcfarland@gmail.com>
2013-01-03 17:53:23 -0600

    sidebar is rendered to #sidebar

--- client/js/support/AppView.js
+++ client/js/support/AppView.js
@@ -6,29 +6,29 @@ define(['jquery', 'underscore', 'backbone'], function($, _, Backbone) {
 
   function AppView() {
 
+    /**
+     * Manages the toolbar creation and destruction
+     */
+
     this.showToolbar = function(view, args) {
-      var self = this;
-
-      if(this.currentToolbar && this.currentToolbarView) {
-        if(this.currentToolbarView !== view) {
-          this.currentToolbar.dispose();
-          this.currentToolbar = new view(args);
-          this.currentToolbarView = view;
-          this.currentToolbar.render();
-          $('body').append(this.currentToolbar.el);
-        }
-        else {
-          this.currentToolbar.reset();
-        }
+      if(this.currentToolbar) {
+        this.currentToolbar.dispose();
       }
-      else {
-        this.currentToolbarView = view;
-        this.currentToolbar = new view(args);
-        this.currentToolbar.render();
 
-        $('body').append(this.currentToolbar.el);
-      }
+      this.currentToolbar = new view(args);
+      this.currentToolbar.render();
+
+      $('#sidebar').html(this.currentToolbar.el);
     };
+
+    this.showToolbarTab = function(tab) {
+      this.currentToolbar.selectTab(tab);
+    };
+
+    /**
+     * Manages the main application view
+     * destruction and creation
+     */
 
     this.showView = function(view, cb) {
       var self = this;
