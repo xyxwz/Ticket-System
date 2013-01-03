b588bd23bf59e0b07bffd26bdb73358e38de5f33
Matt McFarland <matmcfarland@gmail.com>
2013-01-03 15:59:53 -0600

    add template and view for list creation

---
+++ client/js/views/lists/ListFormView.js
@@ -0,0 +1,75 @@
+      var element = $(e.target);
+
+      ticketer.collections.lists.create({
+        name: element.children('[name="name"]').val(),
+        user: ticketer.currentUser.id
+      },{wait: true});
+
+      this.removeListForm(e);
+
+/**
+ * List form view
+ */
+
+define(['jquery', 'underscore', 'backbone',
+  'BaseView', 'mustache', 'text!templates/lists/ListForm.html'],
+function($, _, Backbone, BaseView, mustache, ListForm) {
+
+  var ListFormView = BaseView.extend({
+    tagName: 'div',
+    className: 'row',
+
+    events: {
+      "click .create": "create",
+      "click .back": "back"
+    },
+
+    initialize: function() {
+
+      // Bindings using the garbage collectors bindTo()
+      _.bindAll(this);
+      this.bindTo(this.collection, 'sync', this.redirect);
+    },
+
+    render: function() {
+      var data;
+
+      data = ticketer.currentUser;
+      data.shortname = data.name.split(' ')[0];
+
+      $(this.el).html(Mustache.to_html(TicketForm, ticketer.currentUser));
+
+      this.bindTo(this, 'viewRendered', this.bindResize);
+
+      return this;
+    },
+
+    create: function(e) {
+      e.preventDefault();
+
+      var self = this;
+      var title = $('[name=title]', this.el).val();
+
+      this.collection.create({
+        title: title
+      },
+      { wait: true });
+    },
+
+    redirect: function(model) {
+      this.dispose();
+      window.history.replaceState({}, document.title, "#tickets/open");
+      ticketer.routers.ticketer.navigate("tickets/" + model.id, true);
+    },
+
+    back: function(e) {
+      e.preventDefault();
+      this.dispose();
+      window.history.back();
+    }
+
+  });
+
+  return ListFormView;
+
+});
