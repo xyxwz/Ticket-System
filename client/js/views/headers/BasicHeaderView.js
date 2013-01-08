d57be4595b73f2797766a58bf50ff8234d9a7980
Matt McFarland <matmcfarland@gmail.com>
2013-01-08 16:52:37 -0600

    add basic header view

---
+++ client/js/views/headers/BasicHeaderView.js
@@ -0,0 +1,37 @@
+/**
+ * A basic header view
+ */
+
+define(['jquery', 'underscore', 'mustache', 'BaseView',
+  'text!templates/headers/BasicHeader.html'],
+function($, _, mustache, BaseView, HeaderTmpl) {
+
+  var BasicHeader = BaseView.extend({
+    className: 'view-header',
+    events: {
+      "click button[data-route]": "navigateTo"
+    },
+
+    initialize: function() {
+      _.bindAll(this);
+    },
+
+    render: function() {
+      this.$el.html(Mustache.to_html(HeaderTmpl, {
+        title: this.options.title,
+        route: this.options.route
+      }));
+
+      return this;
+    },
+
+    navigateTo: function(e) {
+      e.preventDefault();
+
+      var target = $(e.currentTarget).data('route');
+      ticketer.routers.ticketer.navigate(target, true);
+    }
+  });
+
+  return BasicHeader;
+});
