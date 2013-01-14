7d20b8a651dbd58675db43bb88134577edb0c70c
Cody Stoltman <particlebanana@gmail.com>
2013-01-13 20:03:17 -0600

    add a full header

---
+++ client/js/views/headers/MainHeaderView.js
@@ -0,0 +1,88 @@
+/**
+ * View Dependencies
+ */
+
+define(['jquery', 'underscore', 'mustache', 'BaseView',
+  'views/widgets/TagFormView',
+  'text!templates/headers/MainHeader.html',
+  'text!templates/headers/UserOptions.html',
+  'dropdowns'],
+function($, _, mustache, BaseView, TagWidget, tmpl_Header, tmpl_Options) {
+
+  /**
+   * A basic header view
+   *
+   * @param {String} route
+   * @param {String} title
+   */
+
+  var MainHeader = BaseView.extend({
+    className: 'header',
+    events: {
+      "click a[data-action]": "preventDefault",
+      "click a[data-route]": "navigateTo",
+      "click a[data-action='refresh']": "refresh",
+      "click a[data-action='new-tag'].active": 'showTagWidget'
+    },
+
+    initialize: function() {
+      var self = this;
+      _.bindAll(this);
+
+      ticketer.EventEmitter.on('session:set', function() {
+        self.addUserOptions();
+      });
+    },
+
+    render: function() {
+      this.$el.html(Mustache.to_html(tmpl_Header));
+
+      return this;
+    },
+
+    addUserOptions: function() {
+      var data = {};
+
+      data.user = ticketer.currentUser;
+      this.$el.append(Mustache.to_html(tmpl_Options, data));
+    },
+
+    preventDefault: function(e) {
+      e.preventDefault();
+    },
+
+    navigateTo: function(e) {
+      e.preventDefault();
+
+      var target = $(e.currentTarget).data('route');
+      ticketer.routers.ticketer.navigate(target, true);
+    },
+
+    refresh: function() {
+      ticketer.collections.openTickets.reset();
+      ticketer.collections.closedTickets.reset();
+      ticketer.collections.users.reset();
+
+      // Emit a `tickets:fetch` event to load ticket data
+      ticketer.sockets.sock.emit('tickets:fetch');
+      ticketer.collections.closedTickets.fetch({ data: { page: 1, status: 'closed' } });
+      ticketer.collections.users.fetch();
+    },
+
+    showTagWidget: function(e) {
+      var element;
+
+      e.preventDefault();
+
+      // Prevent Multiple Widgets
+      $(e.currentTarget).removeClass('active');
+
+      element = $(e.currentTarget).parent();
+
+      this.widget = this.createView(TagWidget);
+      $(element).append(this.widget.render().el);
+    }
+  });
+
+  return MainHeader;
+});
---
+++ client/js/views/widgets/TagFormView.js
@@ -0,0 +1,78 @@
+/**
+ * Widget Dependencies
+ */
+
+define(['jquery', 'underscore', 'backbone',
+  'BaseView', 'mustache',
+  'text!templates/widgets/TagForm.html'],
+function($, _, Backbone, BaseView, mustache, tmpl_TagForm) {
+
+  /**
+   * Widget to Create Tags/Tasks
+   *
+   * @param {Backbone.Model} model ticket model
+   * @param {Backbone.Collection} collection users to search from
+   */
+
+  var TagFormView = BaseView.extend({
+    className: 'tags-form',
+    events: {
+      "click .color-picker li": "setColor",
+      "keypress input[type=text]":  "createOnEnter"
+    },
+
+    initialize: function() {
+      _.bindAll(this);
+    },
+
+    render: function() {
+      var data = {
+        colors: ticketer.colors
+      };
+
+      this.$el.html(Mustache.to_html(tmpl_TagForm, data));
+      return this;
+    },
+
+    setColor: function(e) {
+      var element = $(e.currentTarget);
+          value = element.data('value');
+
+      this.color = value;
+
+      // Add Active Class to show it's selected
+      this.$el.find('li.active').removeClass('active');
+      element.addClass('active');
+
+      $('input[type=text]').attr('disabled', false);
+    },
+
+    /**
+     * When enter is pressed create a new comment model
+     * and resize the textarea to 23px with no content
+     *
+     * @param {jQuery Event} e
+     */
+
+    createOnEnter: function(e) {
+      var element = $('input[type=text]', this.$el);
+
+      if (e.keyCode != 13) { return; }
+      if (e.keyCode === 13 && !e.ctrlKey) {
+        e.preventDefault();
+
+        var self = this;
+
+        ticketer.collections.lists.create({
+          color: this.color,
+          name: element.val()
+        }, { wait: true });
+
+        this.dispose();
+        $("header a[data-action='new-tag']").addClass('active');
+      }
+    }
+  });
+
+  return TagFormView;
+});
