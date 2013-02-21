65ef367d4fa2be7c2280f44a5a409a8a875b902c
Matt McFarland <matmcfarland@gmail.com>
2013-02-21 11:49:52 -0600

    add updated r.js build config, including almond

--- client/js/app.build.js
+++ client/js/app.build.js
@@ -1,32 +1,29 @@
-({
-  appDir: "./",
-  baseUrl: "./",
-  dir: "./release",
-  paths: {
-    'jquery':           'empty:',
-    'backbone':         'libs/backbone-0.9.10',
-    'underscore':       'libs/lodash',
-    'text':             'libs/require/text',
-    'mustache':         'libs/mustache',
-    'marked':           'libs/marked',
-    'moment':           'libs/moment',
-    'dropdowns':        'libs/plugins/bootstrap.dropdowns',
-    'autoresize':       'libs/plugins/jquery.autoresize',
-    'spin':             'libs/spin',
-    'Mousetrap':        'libs/mousetrap',
-    'EventSource':      'libs/eventsource',
-    'Shortcuts':        'support/Shortcuts',
-    'Sync':             'support/Sync',
-    'BaseView':         'support/GarbageCollector',
-    'AppView':          'support/AppView',
-    'ServerEvents':     'support/ServerEvents',
-    'AppCache':         'support/AppCache'
+{
+  "baseUrl": "./",
+  "name": "libs/almond",
+  "include": ["main"],
+  "out": "./release/bundle.js",
+  "paths": {
+    "jquery":           "empty:",
+    "backbone":         "libs/backbone-0.9.10",
+    "underscore":       "libs/lodash",
+    "text":             "libs/require/text",
+    "mustache":         "libs/mustache",
+    "marked":           "libs/marked",
+    "moment":           "libs/moment",
+    "dropdowns":        "libs/plugins/bootstrap.dropdowns",
+    "autoresize":       "libs/plugins/jquery.autoresize",
+    "spin":             "libs/spin",
+    "Mousetrap":        "libs/mousetrap",
+    "EventSource":      "libs/eventsource",
+    "Shortcuts":        "support/Shortcuts",
+    "Sync":             "support/Sync",
+    "BaseView":         "support/GarbageCollector",
+    "AppView":          "support/AppView",
+    "ServerEvents":     "support/ServerEvents",
+    "AppCache":         "support/AppCache"
   },
-  optimize: "uglify",
-  findNestedDependencies: true,
-  modules: [
-    {
-      name: "main"
-    }
-  ]
-})
+  "optimize": "uglify",
+  "preserveLicenseComments": false,
+  "findNestedDependencies": true
+}
