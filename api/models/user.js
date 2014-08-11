778c626c66374267889e958310df4c2fce423106
Kirby <kirby.k6@gmail.com>
2014-08-11 10:04:47 -0500

    Add email during authorization

--- api/models/user.js
+++ api/models/user.js
@@ -207,6 +207,7 @@ module.exports = function(app) {
       }
       else {
         model.access_token = access_token;
+        if(profile.email) {model.email = profile.email;}
         model.save(function(err, user) {
           if(err) return cb(err);
           return cb(null, user);
