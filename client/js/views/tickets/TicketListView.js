eb649bd69e7488ce5adf1768a35184e52ea6bc04
Matt McFarland <matmcfarland@gmail.com>
2014-09-25 11:57:51 -0500

    update ticket collection with new ticket sse logic

--- client/js/views/tickets/TicketListView.js
+++ client/js/views/tickets/TicketListView.js
@@ -129,17 +129,6 @@ function($, _, Backbone, BaseView, TicketView) {
     selectTicket: function(id) {
       this.$el.find('.active').removeClass('active');
       this.$el.find('.ticket[data-id="' + id + '"]').addClass('active');
-    },
-
-    // TODO: Fix this obscure check...
-    newTicket: function(data) {
-      if(data.status === this.collection.status() &&
-          this.viewFilter(data)) {
-        this.collection.add(data);
-        console.log("Ticket added after notification");
-        console.log(data);
-        console.log(this.viewFilter.toString());
-      }
     }
 
   });
